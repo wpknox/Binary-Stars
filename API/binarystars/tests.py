fa440291c6a2f5a14018f357e9d41573d84d2fd4
EthanVDub <ehv@iastate.edu>
2021-03-04 11:17:47 -0600

    Create basic test

--- API/binarystars/tests.py
+++ API/binarystars/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
-
+from myapp.models import BinaryStars
+import random
 # Create your tests here.
+
+
+class BinaryStarTestCase(TestCase):
+    def setup(self):
+        BinaryStars.objects.create(
+            mass_1=random.uniform(0, 1)
+            mass_2=random.uniform(0, 1)
+            lumin_1=random.uniform(0, 1)
+            lumin_2=random.uniform(0, 1)
+            porb=random.uniform(0, 1)
+            time_id=random.uniform(0, 1)
+        )
+
+    def automatic_pass(self):
+        self.assertEqual(1, 1)
