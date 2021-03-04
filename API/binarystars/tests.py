fda322594a314f7e8b55edf8f545ce18307fbc9a
EthanVDub <ehv@iastate.edu>
2021-03-04 13:52:58 -0600

    Added automatic test pass

--- API/binarystars/tests.py
+++ API/binarystars/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from myapp.models import BinaryStars
+from binarystars.models import BinaryStars
 import random
 # Create your tests here.
 
@@ -7,13 +7,13 @@ import random
 class BinaryStarTestCase(TestCase):
     def setup(self):
         BinaryStars.objects.create(
-            mass_1=random.uniform(0, 1)
-            mass_2=random.uniform(0, 1)
-            lumin_1=random.uniform(0, 1)
-            lumin_2=random.uniform(0, 1)
-            porb=random.uniform(0, 1)
-            time_id=random.uniform(0, 1)
+            mass_1=random.uniform(0, 1),
+            mass_2=random.uniform(0, 1),
+            lumin_1=random.uniform(0, 1),
+            lumin_2=random.uniform(0, 1),
+            porb=random.uniform(0, 1),
+            time_id=random.uniform(0, 1),
         )
 
-    def automatic_pass(self):
+    def test_automatic_pass(self):
         self.assertEqual(1, 1)
